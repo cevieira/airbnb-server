7a9684252d33ee27646f940b900c016bb1a5bff7
cevieira <ce.vieira98@gmail.com>
2019-01-22 19:53:34 -0200

    Upload e Geolocalização

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -1,49 +1,40 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-const Property = use('App/Models/Property')
-/**
- * Resourceful controller for interacting with properties
- */
-class PropertyController {
-  /**
-   * Show a list of all properties.
-   * GET properties
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index () {
-    const properties = Property.all()
 
+const Property = use('App/Models/Property')
+
+class PropertyController {
+ 
+  async index () {
+    const {latitude, longitude } = request.all()
+
+    const properties = Property.query()
+    .with('images')
+    .nearBy(latitude, longitude, 10)
+    .fetch()
+    
     return properties
 
   }
 
-    /**
-   * Create/save a new property.
-   * POST properties
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+
+  async store ({ auth, request, response }) {
+
+    const { id } = auth.user
+
+    const data = request.only([
+      'title',
+      'adress',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+  
+    const property = await Property.create({ ...data, user_id: id })
+  
+    return property
   }
 
-  /**
-   * Display a single property.
-   * GET properties/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show ({ params }) {
     const property = await Property.findOrFail(params.id)
 
@@ -52,25 +43,25 @@ class PropertyController {
     return property
   }
 
-  /**
-   * Update property details.
-   * PUT or PATCH properties/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+  
+    const data = request.only([
+      'title',
+      'adress',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+  
+    property.merge(data)
+  
+    await property.save()
+  
+    return property
   }
 
-  /**
-   * Delete a property with id.
-   * DELETE properties/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+
   async destroy ({ params, auth, response }) {
     const property = await Property.findOrFail(params.id)
 
--- app/Models/Property.js
+++ app/Models/Property.js
@@ -2,6 +2,7 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Database = use('Database')
 
 class Property extends Model {
     user () {
@@ -11,6 +12,18 @@ class Property extends Model {
     images (){
         return this.hasMany('App/Model/Image')
     }
+
+    static scopeNearBy(query, latitude, longitude, distance){
+       const haversine = `(6371 * acos(cos(radians(${latitude}))
+                            * cos(radians(latitude))
+                            * cos(radians(longitude)
+                            - radians(${longitude}))
+                            + sin(radians(${latitude}))
+                            * sin(radians(latitude))))`
+        return query
+        .select('*', Database.raw(`${haversine} as distance`))
+        .whereRaw(`${haversine} < ${distance}`)
+    }   
 }
 
 module.exports = Property
